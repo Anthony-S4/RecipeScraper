cf361dc9c620383a8951d8fc90b9fc17c9a2ad7b
Anthony Safo <anthony.s4f@gmail.com>
2023-06-30 22:00:13 -0500

    Updated index.js to take in user input and return shipped and non shipped items array.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import * as cheerio from 'cheerio';
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 //function that will take in link, then return recipe items, currently however it just acquires all recipe items from a hard coded link
 async function getItems(a) { 
     try {
@@ -11,39 +14,49 @@ async function getItems(a) {
       //cheerio.load is used to load the HTML string and transform it into a cheerio object, this object can be used to traverse the DOM
       const $ = cheerio.load(body);
       //$name is convention, here we use the cheerio object to select al elemnts that are descendants specified as [data-test-id="ingredient-item-shipped"] of the attribute [data-test-id= "ingredients-list"] 
-      const $wrapper4 = $('[data-test-id= "ingredients-list"] [data-test-id="ingredient-item-shipped"]');
+      const $shipped = $('[data-test-id= "ingredients-list"] [data-test-id="ingredient-item-shipped"]');
+      const $not_shipped = $('[data-test-id= "ingredients-list"] [data-test-id="ingredient-item-not-shipped"]');
       //We need to create an array to hold each element of the wrapper 
-      let uitems = [];
-
+      let shipped  = [];
+      let not_shipped  = [];
       //iterate through the cherio object wrapper and push each element into our array
-      $wrapper4.each((i, div) => {
-        //console.log($(div).text());
-        uitems.push($(div).text());
+      $shipped.each((i, div) => {
+        shipped.push($(div).text());
     });
+    $not_shipped.each((i, div) => {
+      not_shipped.push($(div).text());
+  });
     //Since the sytax of the website does not place a space between the unit and ingredient, insert a space manually below
-    for(let i=0; i<uitems.length; i++)
-    {
-      if(uitems[i].includes("ounce"))
-      {
-        //create temp result variable b/c string immutable, but arrays are not so just set array index value to new string
-        let result = uitems[i].replace("ounce", "ounce" + " ")
-        uitems[i] = result
-      }
-      if(uitems[i].includes("unit"))
-      {
-        let result = uitems[i].replace("unit", "unit" + " ")
-        uitems[i] = result
-      }
-      if(uitems[i].includes("tablespoon"))
-      {
-        let result = uitems[i].replace("tablespoon", "tablespoon" + " ")
-        uitems[i] = result
-      }
-    }
-    //print
-    console.log(uitems)
+    shipped = itemsFormat(shipped)
+    not_shipped = itemsFormat(not_shipped)
+    //combine shipped and non shipped ingredients arrays
+    let ingredients = shipped.concat(not_shipped)
+    
+    //print result
+    console.log(ingredients)
+
     } catch (error) {
       console.log(error);
     }
   }
-getItems('https://www.hellofresh.com/recipes/peppercorn-steak-w06-5857fcd16121bb11c124f383');
+
+//function will take in an array of items and format them so there is a space between each unit of measurement and the ingredient
+function itemsFormat(items){
+  //holder array to be returned b/c replace does not modify original array
+  let final = items 
+  let filter = ["ounce","unit","tablespoon","teaspoon","cup"]
+  for(let i=0; i<items.length; i++)
+  {
+      for(let j=0; j<filter.length; j++)
+      {
+          if(items[i].includes(filter[j]))
+          {
+              final[i]= items[i].replace(filter[j], filter[j] + " ")
+          }
+      }
+  }
+  return final
+}
+
+//var recipe = prompt("What recipe would you like to retrieve? ");
+getItems('https://www.hellofresh.com/recipes/sheet-pan-italian-organic-beef-meatloaves-649c8afbd54520eb5aba9c1b');
